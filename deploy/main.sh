13db84bc195084bd096bf96ed012ed2db1e1a2c3
Shoeb Chikte <shoeb751@gmail.com>
2021-08-04 19:28:46 +0530

    Adding vimrc allowing some basic stuff

--- deploy/main.sh
+++ deploy/main.sh
@@ -2,7 +2,9 @@
 
 # you need to be in this directory to run this command
 # TODO: Make this better
+[[  "$(pwd)" != "$(realpath $(dirname $0))" ]] && echo "Need to be in directory containing script" && exit 1
 
 ln -sf `pwd`/../main/bashrc ~/.bashrc
 ln -sf `pwd`/../tmux/.tmux/.tmux.conf ~/.tmux.conf
 ln -sf `pwd`/../tmux/.tmux.conf.local ~/.tmux.conf.local
+ln -sf `pwd`/../main/main.vim ~/.vimrc
---
+++ main/main.vim
@@ -0,0 +1,10 @@
+" i never want vim messing with me pasting stuff, ever
+set paste
+
+" tabs should always be spaces, mostly 4
+set tabstop=4
+"set softtabstop=0 noexpandtab
+set shiftwidth=4
+set expandtab
+" use retab to convert tabs to spaces
+" when working with the file
